2ee358239b34655296ff941ab7d07868e0201833
ignacio <ignaciofranciscovigo@gmail.com>
2025-01-11 20:54:26 -0300

    Ex 6.20

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,60 @@
-import AnecdoteForm from './components/AnecdoteForm'
-import Notification from './components/Notification'
+import anecdotesService from "./services/anecdotes";
+import AnecdoteForm from "./components/AnecdoteForm";
+import Notification from "./components/Notification";
+import { useQuery } from "@tanstack/react-query";
 
 const App = () => {
+  // Retrieve anecdotes from server using React Query (tanstack)
+  const anecdotesQuery = useQuery({
+    queryKey: ["anecdotes"],
+    queryFn: anecdotesService.getAll,
+  });
 
-  const handleVote = (anecdote) => {
-    console.log('vote')
+  // When loading
+  if (anecdotesQuery.isPending) {
+    return <div>Loading data from server...</div>;
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  // When error
+  if (anecdotesQuery.isError) {
+    return <div>There was an error fetching anecdotes from the server</div>;
+  }
+
+  // When success
+  const anecdotes = anecdotesQuery.data;
+
+  console.log(anecdotes);
+
+  const handleVote = (anecdote) => {
+    console.log("vote");
+  };
+
+  // const anecdotes = [
+  //   {
+  //     content: "If it hurts, do it more often",
+  //     id: "47145",
+  //     votes: 0,
+  //   },
+  // ];
 
   return (
     <div>
       <h3>Anecdote app</h3>
-    
+
       <Notification />
       <AnecdoteForm />
-    
-      {anecdotes.map(anecdote =>
+
+      {anecdotes.map((anecdote) => (
         <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
+          <div>{anecdote.content}</div>
           <div>
             has {anecdote.votes}
             <button onClick={() => handleVote(anecdote)}>vote</button>
           </div>
         </div>
-      )}
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
